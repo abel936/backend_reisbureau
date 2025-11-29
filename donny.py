a434af226eee18389a3f345b92b127c532df14e6
Donny Luu <donnyluu@acknowledge.nl>
2025-11-29 14:22:21 +0100

    tweede query en tweede route toegevoegd

--- donny.py
+++ donny.py
@@ -1,5 +1,11 @@
 from connect_with_db import get_connection
-from decimal import Decimal
+from flask import request
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 #basic query voor data luchthavens
 def start():
@@ -16,20 +22,29 @@ def start():
     connection.close()
     return data
 
-#beschikbare aankomstluchthavens vanaf vertrekluchthaven
-def get_destinations_from_departure(iata_code):
+#informatie voor vluchtmogelijkheden
+def get_destinations_from_departure():
+    payload = request.get_json()
+    iata_code = payload.get("iata_code")
     connection = get_connection()
     cursor = connection.cursor()
     cursor.execute("""
-        SELECT 
+        SELECT DISTINCT
             a2.iata_code,
             a2.name,
             a2.latitude,
-            a2.longitude
+            a2.longitude,
+            MIN(f.base_price) AS min_price
         FROM dbo.Flights f
-        JOIN dbo.Airports a1 ON f.departure_airport_id = a1.id
-        JOIN dbo.Airports a2 ON f.arrival_airport_id   = a2.id
-        WHERE a1.iata_code = ?;
+        JOIN dbo.Airports a1 ON f.departure_airport_id = a1.airport_id
+        JOIN dbo.Airports a2 ON f.arrival_airport_id   = a2.airport_id
+        WHERE a1.iata_code = ?
+            AND f.status = 'SCHEDULED'
+        GROUP BY
+            a2.iata_code,
+            a2.name,
+            a2.latitude,
+            a2.longitude;
     """, (iata_code,))
     rows = cursor.fetchall()
     keys = [col[0] for col in cursor.description]
